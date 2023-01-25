ca57e4c98e21e3e0b1aa9b7064b0293866b06e13
unknown <marcello.maio1@gmail.com>
2023-01-25 21:11:16 +0000

    using fetch

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const https = require('https');
 const apiKey = process.env.AWS_API_KEY;
 
-const options = {
-  hostname: 'c44r3fzrqc.execute-api.us-east-1.amazonaws.com',
-  path: '/sql_test_api_stage/test2resource',
-  method: 'GET',
+fetch("https://c44r3fzrqc.execute-api.us-east-1.amazonaws.com/sql_test_api_stage/test2resource", {
+  method: "GET",
   headers: {
-    'x-api-key': apiKey
+    "x-api-key": apiKey
   }
-};
-
-
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-  let data = '';
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-  res.on('end', () => {
-    const testBackend = document.getElementById('test-backend');
+})
+  .then(response => {
+    if (response.ok) {
+        return response.text();
+    }
+    throw new Error("Request failed");
+  })
+  .then(data => {
+    const testBackend = document.getElementById("test-backend");
     testBackend.innerHTML = data;
+  })
+  .catch(error => {
+    console.log(error);
   });
-});
 
-req.on('error', (error) => {
-  console.error(error);
-});
-
-req.end();
